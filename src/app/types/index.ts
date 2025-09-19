3418a0ca46735abd837fd32f9c9b18f2c296e5a3
Salem Souleymane NOUHOU <salem.nouhou@epitech.eu>
2025-09-19 19:02:54 +0100

    FIRST COMMIT

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,6 +1,2 @@
 export * from "./button";
 export * from "./card";
-export * from "./HamburgerMenu";
-export * from "./ImageSkeleton";
-export * from "./Benefits";
-export * from "./SuccessStories";
